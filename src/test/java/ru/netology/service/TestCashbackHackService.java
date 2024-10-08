e3b31267906b9d24080a9206e5e922c04f70b49d
Vitaliy K. <begemot39@mail.ru>
2024-10-08 20:17:53 +0300

    Added junit 4.13 and test

---
+++ src/test/java/ru/netology/service/TestCashbackHackService.java
@@ -0,0 +1,47 @@
+package ru.netology.service;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCashbackHackService {
+
+    CashbackHackService cash = new CashbackHackService();
+
+    @Test
+    public void testCashBackHackerPositive() { // Случайное корректное значение суммы.
+
+        int act = cash.remain( 848 );
+        int exp = 152;
+
+        Assert.assertEquals( act, exp ) ;
+    }
+
+    @Test
+    public void testCashBackHackerPositive1() { // Минимальное корректное значение суммы.
+
+        int act = cash.remain( 999 );
+        int exp = 1;
+
+        Assert.assertEquals( act, exp ) ;
+    }
+
+    @Test
+    public void testCashBackHackerPositive999() { // Максимальное корректное значение суммы.
+
+        int act = cash.remain( 1 );
+        int exp = 999;
+
+        Assert.assertEquals( act, exp ) ;
+    }
+
+    @Test
+    public void testCashBackHackerBoundary() { // Передача одинаковой с границей суммы.
+
+        int act = cash.remain( 1000 );
+        int exp = 1000;
+
+        Assert.assertEquals( act, exp ) ;
+    }
+
+}
